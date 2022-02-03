13ff8536ac31f911b026dbb3aafc7bb81269a44c
Darksaid <74002782+semenovaa9205@users.noreply.github.com>
2022-02-03 17:25:37 +0300

    add textboot
    text boot for python

---
+++ textboot.py
@@ -0,0 +1,32 @@
+from email import message
+import pyaudio
+def listen_comand():
+    
+    
+    return input("Cкажите вашу команду:")
+def do_this_comand(message):
+     message=message.lower()
+     if "как тебя зовут?" in message:
+         say_message("меня зовут лиза!")
+     elif "привет лиза" in message:
+         say_message("приветик") 
+     elif "пока лиза": 
+         say_message("пока солнышко!")
+         exit()
+     else :
+      say_message('команнда не распознана!!')
+def say_message(message):
+    print(message)
+if __name__=='__main__':
+     while True:
+      comand=listen_comand()
+      do_this_comand(comand)
+
+
+
+
+
+    
+        
+     
+ 
