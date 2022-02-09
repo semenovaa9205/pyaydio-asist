2a290010d3da88ea2af2f3ca02827ff61be0c082
Darksaid <74002782+semenovaa9205@users.noreply.github.com>
2022-02-09 14:02:28 +0300

    add json

--- winaudiobot.py
+++ winaudiobot.py
@@ -1,10 +1,10 @@
 import os
-
+import json
 from gtts import gTTS
 import time
 import random
 import speech_recognition as sr 
-
+from winsound import *
 def listen_comand():
  r=sr.Recognizer()
  with sr.Microphone() as source:
@@ -24,24 +24,22 @@ def listen_comand():
     #return input("Cкажите вашу команду:")
 def do_this_comand(message):
      message=message.lower()
-     if "как тебя зовут?" in message:
-         say_message("меня зовут лиза!")
+
+     if "кgак тебя зовут?" in message:
+       say_message("меня зовут лиза!")
      elif "привет лиза" in message:
-         say_message("приветик") 
+          say_message("приветик") 
      elif "пока лиза": 
          say_message("пока солнышко!")
          exit()
      else :
       say_message('команнда не распознана!!')
-
 def say_message(message):
- voice=gTTS(message, lang="ru")
- file_voice_name="D:\pytich\saundasist\_iisoundvoice_"+str(time.time())+"_"+str(random.randint(0,10000))+".mp3" 
- voice.save(file_voice_name)
- 
- os.startfile(file_voice_name)
- 
- print("Голосовой ассистент"+message)
+  voice=gTTS(message, lang="ru")
+  file_voice_name="D:\pytich\saundasist\_iisoundvoice_"+str(time.time())+"_"+str(random.randint(0,10000))+".mp3" 
+  voice.save(file_voice_name)
+  PlaySound.playsound(file_voice_name)
+  print("Голосовой ассистент"+message)
 if __name__=='__main__':
      while True:
       comand=listen_comand()
