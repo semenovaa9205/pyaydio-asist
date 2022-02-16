24af3056c98950977d240d66adf2d254f63e27eb
Darksaid <74002782+semenovaa9205@users.noreply.github.com>
2022-02-16 20:38:45 +0300

    add udiobot

--- audiobotv2.py
+++ audiobotv2.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 from ast import If
 from email import message
 import os
@@ -79,3 +80,77 @@ if __name__=='__main__':
         
      
  
+=======
+from ast import If
+from email import message
+import os
+import json
+from gtts import gTTS
+import time
+import random
+import speech_recognition as sr 
+
+def listen_comand():
+ r=sr.Recognizer()
+ with sr.Microphone() as source:
+  print("Cкажите вашу команду:")
+  audio=r.listen(source)
+  try:
+    spich=r.recognize_google(audio,language="ru-RU")
+    print("вы сказали"+spich)
+    return spich
+  except sr.UnknownValueError:
+     print("неизвестнная ошика")
+  except sr.RequestError :
+      print("ошибка обработки файла")
+
+    
+    
+    #return input("Cкажите вашу команду:")
+def do_this_comand(message):
+     message=message.lower()
+    
+     db={
+     "intents":{
+          'hello':{
+           "examples":["привет","хай"],
+           "responses":["приветик","хающки"]
+
+          },
+
+        'poka':{
+         
+           "examples" :["пока","до встречи"],
+           "responses":["пока солнкнышко!","я буду вас ждать хозяин!"]
+          },
+          'name':{
+             "examples":["как тебя зовут?","как твоё имя?","скажи своё имя"],
+              "resposes":["меня зовут лиза ","лизанька", "елизавета"]
+          }
+             } 
+             }
+     for intent in db["intents"].keys():
+      for example in db["intents"][intent]["examples"]:
+                      if message==example:
+                        return  say_message(random.choice(db["intents"][intent]["responses"]))
+                      else:
+                       return say_message('команнда не распознана!!')    
+                           
+                           
+   
+      
+                        
+      
+            
+def   say_message(message):
+  voice=gTTS(message, lang="ru")
+  file_voice_name="D:\pytich\saundasist\_iisoundvoice_"+str(time.time())+"_"+str(random.randint(0,10000))+".mp3" 
+  voice.save(file_voice_name)
+  os.startfile(file_voice_name)
+  print("Голосовой ассистент"+message)
+if __name__=='__main__':
+ while True:
+       comand=listen_comand()
+       do_this_comand(comand)
+      
+>>>>>>> Stashed changes
