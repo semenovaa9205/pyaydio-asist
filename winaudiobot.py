4c4ec55be51ebccfaaf2a249b3ecca41b759ff61
Darksaid <74002782+semenovaa9205@users.noreply.github.com>
2022-04-19 17:28:42 +0300

    delete winsaund

--- winaudiobot.py
+++ winaudiobot.py
@@ -4,7 +4,7 @@ from gtts import gTTS
 import time
 import random
 import speech_recognition as sr 
-from winsound import *
+
 def listen_comand():
  r=sr.Recognizer()
  with sr.Microphone() as source:
